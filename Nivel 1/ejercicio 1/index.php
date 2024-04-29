328222882d35721f8908dd2f9cdd2cb3ad6e99f9
srbandit <149817789+srbandit@users.noreply.github.com>
2024-04-29 17:35:21 +0200

    V2

--- Nivel 1/ejercicio 1/index.php
+++ Nivel 1/ejercicio 1/index.php
@@ -1,9 +1,6 @@
 <?php
 
 $nuevoArray = [1, 2, 3, 4, 5];
-echo $nuevoArray[0];
-echo $nuevoArray[1];
-echo $nuevoArray[2];
-echo $nuevoArray[3];
-echo $nuevoArray[4];
-?>
+foreach ($nuevoArray as $x) {
+    echo "$x <br>";
+}
